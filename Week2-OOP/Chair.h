bb9db9d04b7322736f5bb75e910a095e3d574afe
Eric Charnesky <EricCharnesky@gmail.com>
2025-01-16 16:38:50 -0500

    operator overloading

---
+++ Week2-OOP/Chair.h
@@ -0,0 +1,83 @@
+#include <string>
+
+using namespace std;
+
+#pragma once
+class Chair {
+
+private:
+	string color;
+	int heightInCM;
+
+public:
+	Chair(string color, int heightInCM) {
+		this->color = color;
+		this->heightInCM = heightInCM;
+	}
+
+	/*
+	*
+	* um gpt
+	write get and set methods for the attributes of this class
+
+class Chair {
+
+private:
+	string color;
+	int heightInCM;
+
+public:
+	Chair(string color, int heightInCM) {
+		this->color = color;
+		this->heightInCM = heightInCM;
+	}
+	*/
+
+	string getColor() const {
+		return color;
+	}
+
+	void setColor(const string& newColor) {
+		color = newColor;
+	}
+
+	int getHeightInCM() const {
+		return heightInCM;
+	}
+
+	void setHeightInCM(int newHeightInCM) {
+		if (newHeightInCM > 0) {
+			heightInCM = newHeightInCM;
+		}
+		// TODO - exceptions
+	}
+};
+
+bool operator==(const Chair& lhs, const Chair& rhs) {
+	return (lhs.getHeightInCM() == rhs.getHeightInCM()) &&
+		(lhs.getColor() == rhs.getColor());
+}
+
+bool operator!=(const Chair& lhs, const Chair& rhs) {
+	return !(lhs == rhs);
+}
+
+bool operator<(const Chair& lhs, const Chair& rhs) {
+	return lhs.getHeightInCM() < rhs.getHeightInCM();
+}
+
+bool operator<=(const Chair& lhs, const Chair& rhs) {
+	return lhs.getHeightInCM() <= rhs.getHeightInCM();
+}
+
+// consider being lazy, not <= means it is >
+bool operator>(const Chair& lhs, const Chair& rhs) {
+	return lhs.getHeightInCM() > rhs.getHeightInCM();
+}
+
+bool operator>=(const Chair& lhs, const Chair& rhs) {
+	return lhs.getHeightInCM() >= rhs.getHeightInCM();
+}
+
+
+
--- Week2-OOP/Week2-OOP.cpp
+++ Week2-OOP/Week2-OOP.cpp
@@ -1,8 +1,27 @@
 
-
 #include <iostream>
+
+#include "Chair.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    Chair ericsChair("Red", 50);
+    Chair bobsChair("blue", 50);
+    Chair matchingChair("Red", 50);
+
+    if (ericsChair == bobsChair) {
+        cout << "Equal!" << endl;
+    }
+    else {
+        cout << "not Equal!" << endl;
+    }
+
+    if (ericsChair == matchingChair) {
+        cout << "Equal!" << endl;
+    }
+    else {
+        cout << "not Equal!" << endl;
+    }
 }
